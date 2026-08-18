52e80ea9f6692d7e79030d77e1cfe4c16fd48960
Yann-Olympe <yannmboh@gmail.com>
2026-08-18 23:26:16 +0100

    feat(citizen): implement problem and solution components with styling and data binding

--- frontend/src/app/features/citizen/accueil/components/probleme/probleme.ts
+++ frontend/src/app/features/citizen/accueil/components/probleme/probleme.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './probleme.html',
   styleUrl: './probleme.css',
 })
-export class Probleme {}
+export class Probleme {
+    items = [
+    { icon: '🚗', titre: 'Déplacements coûteux', texte: "Voyager vers votre mairie de naissance engendre des frais de transport importants et une perte de temps précieux." },
+    { icon: '⏳', titre: "Files d'attente interminables", texte: "Les services de l'état civil sont souvent surchargés, nécessitant de longues heures d'attente sur place." },
+    { icon: '⚠️', titre: "Risques d'erreurs", texte: "La recopie manuelle des informations augmente le risque d'erreurs sur vos documents officiels finaux." }
+  ];
+}
--- frontend/src/app/features/citizen/accueil/components/solution/solution.ts
+++ frontend/src/app/features/citizen/accueil/components/solution/solution.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './solution.html',
   styleUrl: './solution.css',
 })
-export class Solution {}
+export class Solution {
+   etapes = [
+    { num: 1, acteur: 'Vous', texte: "Initiez la demande en ligne en quelques minutes, depuis n'importe où, sur votre téléphone ou ordinateur." },
+    { num: 2, acteur: 'CivilPass', texte: "Vérifie, sécurise et transmet instantanément votre dossier numérique à l'officier d'état civil compétent." },
+    { num: 3, acteur: 'Votre Mairie', texte: "Traite officiellement la demande et expédie le document final certifié vers la mairie de retrait choisie." }
+  ];
+}
