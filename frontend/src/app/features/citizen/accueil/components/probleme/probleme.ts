c8bf2e58868e8452fb2093e62b6d404a31ce7605
Yann-Olympe <yannmboh@gmail.com>
2026-08-19 00:18:36 +0100

    feat(citizen): update probleme component styles and structure, enhance icon usage

--- frontend/src/app/features/citizen/accueil/components/probleme/probleme.ts
+++ frontend/src/app/features/citizen/accueil/components/probleme/probleme.ts
@@ -7,9 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './probleme.css',
 })
 export class Probleme {
-    items = [
-    { icon: '🚗', titre: 'Déplacements coûteux', texte: "Voyager vers votre mairie de naissance engendre des frais de transport importants et une perte de temps précieux." },
-    { icon: '⏳', titre: "Files d'attente interminables", texte: "Les services de l'état civil sont souvent surchargés, nécessitant de longues heures d'attente sur place." },
-    { icon: '⚠️', titre: "Risques d'erreurs", texte: "La recopie manuelle des informations augmente le risque d'erreurs sur vos documents officiels finaux." }
-  ];
+  items = [
+      { icon: 'warning', titre: 'Déplacements coûteux', texte: "Voyager vers votre mairie de naissance engendre des frais de transport importants et une perte de temps précieux." },
+      { icon: 'clock', titre: "Files d'attente interminables", texte: "Les services de l'état civil sont souvent surchargés, nécessitant de longues heures d'attente sur place." },
+      { icon: 'pencil', titre: "Risques d'erreurs", texte: "La recopie manuelle des informations augmente le risque d'erreurs sur vos documents officiels finaux." }
+    ];
 }
