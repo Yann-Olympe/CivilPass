f8af7d1390cf6aed138f2b74b04b8b297003941e
Pakistant <stdouanla@gmail.com>
2026-08-20 11:13:38 +0100

    feat(Models):modification et ajouts models notification

--- backend/app/Models/Usager.php
+++ backend/app/Models/Usager.php
@@ -3,14 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Usager extends Model
+class Usager extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
 
-    protected $fillable = ['nom', 'prenom', 'telephone'];
+    protected $fillable = ['nom', 'prenom', 'telephone', 'password'];
+
+    protected $hidden = ['password', 'remember_token'];
+
+    protected function casts(): array
+    {
+        return ['password' => 'hashed'];
+    }
 
     public function demandes(): HasMany
     {
